a2a6e53b3e090dab06016351e89d230dcdcced59
jangomango <86420004+jangodev@users.noreply.github.com>
2022-01-21 12:51:31 -0500

    added pass origin

--- ws-proxy.js
+++ ws-proxy.js
@@ -4,26 +4,21 @@
   const config = JSON.parse(fs.readFileSync('./config.json', {encoding:'utf8'})); 
   
   if (!config.prefix.startsWith('/')) {
-  
-      config.prefix = `/${config.prefix}`;
-      
+      config.prefix = `/${config.prefix}`; 
   }
 
-  
-
   if (!config.prefix.endsWith('/')) {
-  
      config.prefix = `${config.prefix}/`;
-     
   }
 
   btoa = (str) => {
   
       str = new Buffer.from(str).toString('base64');
-      
       return str;
       
   };
+  
+
   
   atob = (str) => {
   
@@ -39,17 +34,55 @@
 
   wss.on('connection', (cli, req) => {
 
-    try {
-
-      const svr = new WebSocket(atob(req.url.toString().replace(`${config.prefix}ws/`, '')));
-
-      svr.on('message', (data) => {
-
-        try { cli.send(data) } catch(err){}
-
+    //try {
+      const svr = new WebSocket(atob(req.url.toString().replace(`${config.prefix}ws/`, '').split('?origin=')[0]), {
+        origin: req.url.split('?origin=')[1]
       });
 
       svr.on('open', () => {
+
+        console.log('proxy opened')
+
+        //try { cli.send(data) } catch(err){}
+        cli.on('message', (data) => {
+
+          svr.send(data.toString())
+    
+        });
+
+        svr.on('message', (data) => {
+
+          cli.send(data)
+    
+        });        
+
+        cli.on('close', (code) => {
+
+          svr.close()//try { svr.close(code); } catch(err) { svr.close(1006)  };
+
+        });
+
+        svr.on('close', (code) => {
+
+          cli.close()//try { cli.close(code); } catch(err) { cli.close(1006)  };
+
+        });
+
+        cli.on('error', (err) => {
+
+          svr.terminate()//try { svr.close(1001); } catch(err) { svr.close(1006) };
+
+        });
+
+        svr.on('error', (err) => {
+
+          cli.terminate()//try { cli.close(1001); } catch(err) { cli.close(1006) };
+
+        });
+
+      });
+
+      /*svr.on('open', () => {
     
         cli.on('message', (data) => {
 
@@ -57,7 +90,7 @@
     
         });
 
-      });
+      });*/
  
 
       cli.on('close', (code) => {
@@ -84,6 +117,6 @@
 
       });
   
-    } catch(err) { cli.close(1001); }
+    //} catch(err) { console.log(err);cli.close(1001); }
   });
  }
